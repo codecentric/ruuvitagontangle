a20a464498fe2c6ffec6441eeae40ebc4f50f1f4
Norbert Schneider <norbert.schneider@codecentric.de>
2018-02-17 20:22:14 +0100

    Parse RuuviTag data

--- src/index.js
+++ src/index.js
@@ -16,6 +16,59 @@ noble.on('warning', function (state) {
     console.log('Warning', state);
 });
 
+const parseRawRuuvi = function(manufacturerDataString){
+    const humidityStart      = 6;
+    const humidityEnd        = 8;
+    const temperatureStart   = 8;
+    const temperatureEnd     = 12;
+    const pressureStart      = 12;
+    const pressureEnd        = 16;
+    const accelerationXStart = 16;
+    const accelerationXEnd   = 20;
+    const accelerationYStart = 20;
+    const accelerationYEnd   = 24;
+    const accelerationZStart = 24;
+    const accelerationZEnd   = 28;
+    const batteryStart       = 28;
+    const batteryEnd         = 32;
+
+    const robject = {};
+
+    let humidity = manufacturerDataString.substring(humidityStart, humidityEnd);
+    humidity = parseInt(humidity, 16);
+    humidity/= 2; //scale
+    robject.humidity = humidity;
+
+    const temperatureString = manufacturerDataString.substring(temperatureStart, temperatureEnd);
+    let temperature = parseInt(temperatureString.substring(0, 2), 16);  //Full degrees
+    temperature += parseInt(temperatureString.substring(2, 4), 16)/100; //Decimals
+    if(temperature > 128){           // Ruuvi format, sign bit + value
+        temperature = temperature-128;
+        temperature = 0 - temperature;
+    }
+    robject.temperature = +temperature.toFixed(2); // Round to 2 decimals, format as a number
+
+    let pressure = parseInt(manufacturerDataString.substring(pressureStart, pressureEnd), 16);  // uint16_t pascals
+    pressure += 50000; //Ruuvi format
+    robject.pressure = pressure;
+
+    let accelerationX = parseInt(manufacturerDataString.substring(accelerationXStart, accelerationXEnd), 16);  // milli-g
+    if(accelerationX > 32767){ accelerationX -= 65536;}  //two's complement
+
+    let accelerationY = parseInt(manufacturerDataString.substring(accelerationYStart, accelerationYEnd), 16);  // milli-g
+    if(accelerationY > 32767){ accelerationY -= 65536;}  //two's complement
+
+    let accelerationZ = parseInt(manufacturerDataString.substring(accelerationZStart, accelerationZEnd), 16);  // milli-g
+    if(accelerationZ > 32767){ accelerationZ -= 65536;}  //two's complement
+
+    robject.accelerationX = accelerationX;
+    robject.accelerationY = accelerationY;
+    robject.accelerationZ = accelerationZ;
+    robject.battery = parseInt(manufacturerDataString.substring(batteryStart, batteryEnd), 16);
+
+    return robject;
+};
+
 const printAdvertisement = function (peripheral) {
     const advertisement = peripheral.advertisement;
     const localName = advertisement.localName;
@@ -86,6 +139,12 @@ const handlePeripheral = function (peripheral) {
     });
 };
 
+const handleRuuviData = function (peripheral) {
+    const manufacturerDataSting = peripheral.advertisement.manufacturerData.toString('hex');
+    console.log(parseRawRuuvi(manufacturerDataSting));
+};
+
+
 noble.on('discover', function (peripheral) {
     if (!isRuuviPeripheral(peripheral)) {
         return;
@@ -96,6 +155,7 @@ noble.on('discover', function (peripheral) {
 
     printAdvertisement(peripheral);
     handlePeripheral(peripheral);
+    handleRuuviData(peripheral);
 });
 
 noble.startScanning();
